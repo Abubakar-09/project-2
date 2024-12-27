715c38f0e026a31024ee4ddd9916ab4b9c8c4c29
AbuBakar Chohan <132102392+Abubakar-09@users.noreply.github.com>
2024-12-27 15:01:05 +0500

    Add files via upload

---
+++ personal_website/app/about/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+      <div className="w-[80vw] m-auto h-screen max-md:pt-16 max-md:pb-16 justify-center max-md:h-auto gap-10 max-md:w-[95vw] py-6 max-md:py-10 flex flex-col">
+        <h1 className='font-extrabold text-4xl'>About Me</h1>
+        <p className='text-md'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad, quos eos earum consequuntur delectus, veniam officia tempore quis ipsum dignissimos mollitia eum sequi quibusdam hic aspernatur inventore autem laboriosam rerum repudiandae debitis error molestias aliquam. Dignissimos necessitatibus ab ullam velit amet, consectetur quia deserunt, architecto quas veritatis ipsa ipsum, aspernatur eligendi distinctio libero beatae laudantium. Aliquam ex veniam officiis magnam, ipsa maxime temporibus a nesciunt inventore tempore minima, optio, consectetur veritatis ducimus quae quos sapiente unde perferendis suscipit eveniet totam cum quibusdam. Nulla blanditiis cupiditate quam. Aliquam, quasi voluptatum. Aut, aliquam. Iusto dicta voluptatum quia animi fugit blanditiis temporibus corporis.</p>
+      </div>
+    </>
+  )
+}
+
+export default page
